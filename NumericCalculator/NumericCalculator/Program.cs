69a6d5d2c97facf6a259aff1c4296718ef04b1e4
Eduardo Franco <efranco@asesoftware.com>
2022-05-10 11:31:00 -0500

    Inclusión de proyecto para definir variables de c#

---
+++ NumericCalculator/NumericCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumericCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int numberOne;
+            int numberTwo;
+
+            Console.WriteLine("Please, input the first number:");
+            numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please, input the second number:");
+            numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            int result = numberOne * numberTwo;
+
+            Console.WriteLine(numberOne + " * " + numberTwo + " = " + result);
+        }
+    }
+}
---
+++ StringExample/StringExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? Please write your full name.");
+            //Read information for terminal. I can use String for variables or var
+            //var fullUserName = Console.ReadLine();
+            string fullUserName = Console.ReadLine();
+
+            //Print value of fullUserName variable
+            Console.WriteLine("Hello " + fullUserName + ", Welcome to Platzi.");
+        }
+    }
+}
